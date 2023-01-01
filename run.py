2fa9a203a306cc46596c62482e8feceda1803e76
shankar2311 <umabo2311@gmail.com>
2023-01-01 11:49:17 +0000

    Create Board class and within class create init method

--- run.py
+++ run.py
@@ -2,3 +2,18 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 player_name = input("Please enter your name:")
+
+class Board:
+    """
+    Main board class. Sets board size, the number of ships,
+    the player's name and the board type (player or computer)
+    """
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+
+data = Board(5, 4, "player", type="player") 
+
+print(data.name)       
