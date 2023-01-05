a78f2f010971df2c78995b0d72b99a37ec7ce458
shankar2311 <umabo2311@gmail.com>
2023-01-05 08:57:23 +0000

    Update wire up API's

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import gspread
-from google oauth2.service_account import Credentials
+from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
