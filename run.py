ecc85f6b796b514965da31dd982e12b720d2a1af
shankar2311 <umabo2311@gmail.com>
2023-01-05 00:30:24 +0000

    Fix indentation

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from random import randint
 
+
 class Board:
     """
     Main board class. Sets board size.
     Has methods for printing board for both player and computer.
     code inspired from Code Institute lessons
     """
-    #Variable to store hit points
+    # Variable to store hit points
     score = 0
 
     def __init__(self, name):
@@ -15,11 +16,10 @@ class Board:
         self.ship_locations = []
         self.add_ships()
         
-
     def add_ships(self):
         """
         In this method we add 4 ships to both player board and computer board
-        Ships in player board are visible and ships in computer board are not shown.
+        Ships in player board are visible and in computer board are not shown
         code is inspired from Code Institute lessons and from youtube.
         """
         p = 0 
@@ -33,7 +33,6 @@ class Board:
                     self.board[row][col] = "@"
                     self.ship_locations.append((row, col))
                     p = p+1
-                   
         else:
 
             while p < 4:
@@ -43,8 +42,7 @@ class Board:
                     self.ship_locations.append((row, col))
                     p = p+1
                     
-            
-    #Below code is taken from Code Institute project portfolio
+    # Below code is taken from Code Institute project portfolio
     def print_board(self):
         print("\n"+self.name+"'s board \n")
         for row in self.board:
@@ -63,12 +61,10 @@ class Board:
                 print("You must enter an integer") 
                 continue 
 
-            if (row >4 and col > 4):
+            if (row > 4 and col > 4):
                 print("Your guess is out of range! Try again..\n")
             else:
                 return (row, col)
-
-                
 
     def computer_guess(self):
         print("\ncomputer is guessing now..")
@@ -76,7 +72,6 @@ class Board:
         col = randint(0, 4)
         print("\nComputer guess is ",(row,col))
         return (row, col)
-
 
     def check_guess(self, row_col, opp_player):
         """
@@ -88,12 +83,10 @@ class Board:
         if row_col in opp_player.ship_locations:
             self.score = self.score + 1
             print("\nHit Successful!. "+self.name+"'s score is ",self.score) 
-            
             opp_player.board[row_col[0]][row_col[1]] = "X"
         else:
             print("\n"+self.name+"'s hit unsuccessful\n")
             opp_player.board[row_col[0]][row_col[1]] = "O" 
-
 
     def new_game():
         """
@@ -109,38 +102,31 @@ class Board:
         print("_" * 35)
         print("\n\n Let's Play\n\n")
 
-
-        #Initiates playerboard and prints the board for the player
+        # Initiates playerboard and prints the board for the player
         player = Board(input("Please enter player name:"))
         player.print_board()
 
-        #Initiates computerboard and prints the board for the computer
+        # Initiates computerboard and prints the board for the computer
         computer = Board("computer")
         computer.print_board()
-        
         """
         Code is inspired from other battleship game's code.
-
         Check the guesses of both player and computer, raises error
         messsage if same row and column is already used.
-
         And displays who the winner is after the game.
         """
-
-        
         while player.score < 4 and computer.score < 4:
 
             while True:
-                p_row_col = player.player_guess()
+                p_rw_cl = player.player_guess()
 
-                if computer.board[p_row_col[0]][p_row_col[1]] not in ['X', 'O']:
+                if computer.board[p_rw_cl[0]][p_rw_cl[1]] not in ['X', 'O']:
 
-                    player.check_guess(p_row_col, computer)
+                    player.check_guess(p_rw_cl, computer)
                     computer.print_board()
                     break
                 else:
                     print("Coordinates already used\n") 
-
 
             while True:
                 c_row_col = computer.computer_guess()
@@ -158,5 +144,4 @@ class Board:
         else:
             print("\nComputer won!!\n")        
 
-        
 Board.new_game()    
